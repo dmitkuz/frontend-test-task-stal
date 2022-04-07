fd14fa96996f9bdfb4cfff1f59c0b0e56e890a9f
kuzaev <63751276+kuzaev@users.noreply.github.com>
2022-04-07 14:02:24 +0400

    add news data

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,9 +1,41 @@
+import { Skeleton, List, Typography } from "antd";
+import { useState, useEffect } from "react";
+
 const News = () => {
+  const [news, setNews] = useState(Array(4).fill({}));
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    setTimeout(() => {
+      fetch("/news.json")
+        .then((response) => response.json())
+        .then(({ articles }) => {
+          setIsLoading(false);
+          setNews(articles);
+        });
+    }, 3000);
+  }, []);
+
   return (
-    <h1>
-      Страница с новостями (любая информация, желательно новости хранить в json
-      формате)
-    </h1>
+    <>
+      <Typography.Title>Новости</Typography.Title>
+      <List
+        itemLayout="horizontal"
+        dataSource={news}
+        renderItem={(item) => (
+          <List.Item>
+            <Skeleton loading={isLoading} active>
+              <List.Item.Meta
+                title={item.title}
+                description={item.description}
+              />
+            </Skeleton>
+          </List.Item>
+        )}
+      />
+    </>
   );
 };
 
