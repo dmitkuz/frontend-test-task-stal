9e30b893d1b98eef811f5654d1d8fd723f386246
kuzaev <63751276+kuzaev@users.noreply.github.com>
2022-04-07 14:47:39 +0400

    fix. React 18

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import store from "./store";
 import "./index.css";
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root"));
+
+root.render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
