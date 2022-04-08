58e2180c6ca6ef5a0329d5128bc45f5a4c792fa7
kuzaev <63751276+kuzaev@users.noreply.github.com>
2022-04-08 10:44:19 +0400

    add logout

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,25 @@
+import { Typography, Button } from "antd";
+import { useDispatch } from "react-redux";
+import { logout } from "../store/auth/actions";
+
 const Profile = () => {
-  return <h1>Страница с произвольным текстом, недоступная без авторизации</h1>;
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
+  return (
+    <>
+      <Typography.Title>Профиль</Typography.Title>
+      <Typography.Paragraph>
+        Страница с произвольным текстом, недоступная без авторизации
+      </Typography.Paragraph>
+      <Button type="primary" onClick={handleLogout}>
+        Выйти
+      </Button>
+    </>
+  );
 };
 
 export default Profile;
